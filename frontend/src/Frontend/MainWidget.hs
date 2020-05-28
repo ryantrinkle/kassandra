9959084566801ae7b4a50181979503ef85a35c9d
Ryan Trinkle <ryan@trinkle.org>
2020-05-28 09:14:50 -0400

    Cleanup

--- frontend/src/Frontend/MainWidget.hs
+++ frontend/src/Frontend/MainWidget.hs
@@ -7,18 +7,13 @@ where
 import qualified Reflex.Dom                    as D
 import qualified Reflex                        as R
 import           Frontend.Types                 ( DragState(NoDrag)
-                                                , AppState(AppState)
-                                                , FilterState(FilterState)
                                                 , AppStateChange
                                                 , WidgetIO
-                                                , StandardWidget
                                                 , DataChange
                                                 )
 
 mainWidget :: WidgetIO t m => m ()
 mainWidget = do
-  time    <- liftIO getZonedTime
-
   rec let (appChangeEvents, _) =
             R.fanThese $ partitionEithersNE <$> stateChanges
       dragDyn <- R.holdDyn NoDrag $ last <$> appChangeEvents
