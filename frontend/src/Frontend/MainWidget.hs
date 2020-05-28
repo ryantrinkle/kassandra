e8c2499e8b033950b90597b8ff174ba266b6ecf9
Ryan Trinkle <ryan@trinkle.org>
2020-05-28 09:19:26 -0400

    Clean up Create2 event source

--- frontend/src/Frontend/MainWidget.hs
+++ frontend/src/Frontend/MainWidget.hs
@@ -7,9 +7,7 @@ where
 import qualified Reflex.Dom                    as D
 import qualified Reflex                        as R
 import           Frontend.Types                 ( DragState(NoDrag)
-                                                , AppStateChange
                                                 , WidgetIO
-                                                , DataChange
                                                 )
 
 mainWidget :: WidgetIO t m => m ()
@@ -17,14 +15,12 @@ mainWidget = do
   rec let (appChangeEvents, _) =
             R.fanThese $ partitionEithersNE <$> stateChanges
       dragDyn <- R.holdDyn NoDrag $ last <$> appChangeEvents
-      (_, stateChanges' :: R.Event t (NonEmpty AppStateChange)) <-
+      (_, stateChanges' :: R.Event t (NonEmpty (Either DragState ()))) <-
         R.runEventWriterT
           (do
             R.tellEvent . fmap one
-               . fmap (_Typed @AppStateChange % _Typed @DataChange % #_CreateTask #) . R.traceEventWith (const "Creating Task2")
-               =<< fmap (, id)
-               <$> ("Click" <$)
-               <$> D.button "Create2"
+               . fmap (\_ -> Right ()) . R.traceEventWith (const "Creating Task2")
+               =<< D.button "Create2"
             D.dyn_ ((D.dyn_ $ pass <$ dragDyn) <$ pure ())
             D.dyn_ ((D.dyn_ $ pass <$ dragDyn) <$ pure ())
             D.dyn_ ((D.dyn_ $ pass <$ dragDyn) <$ pure ())
