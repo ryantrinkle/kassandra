288c76e939f12795563b589254349225ad39484f
Ryan Trinkle <ryan@trinkle.org>
2020-05-28 09:37:17 -0400

    More cleanup

--- frontend/src/Frontend/MainWidget.hs
+++ frontend/src/Frontend/MainWidget.hs
@@ -4,11 +4,25 @@ module Frontend.MainWidget
   )
 where
 
-import Prelude (MonadFix, NonEmpty, Either (..), const, pure, (<$), ($), pass, void, (.), fmap, last, (=<<), partitionEithersNE, (<$>), one)
+import Prelude ( const
+               , pure
+               , (<$)
+               , ($)
+               , void
+               , (.)
+               , fmap
+               , last
+               , (=<<)
+               , (<$>)
+               )
 
 import qualified Reflex.Dom                    as D
 import qualified Reflex as R
 import qualified Reflex.Network as R
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.These (These (..))
+import Data.Either (Either (..), partitionEithers)
+import Control.Monad.Fix (MonadFix)
 
 mainWidget :: (D.DomBuilder t m, MonadFix m, R.PostBuild t m, R.MonadHold t m) => m ()
 mainWidget = do
@@ -18,16 +32,24 @@ mainWidget = do
       (_, stateChanges' :: R.Event t (NonEmpty (Either () ()))) <-
         R.runEventWriterT
           (do
-            R.tellEvent . fmap one
+            R.tellEvent . fmap (:| [])
                . fmap (\_ -> Right ()) . R.traceEventWith (const "Creating Task2")
                =<< D.button "Create2"
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
-            void $ R.networkView $ (void $ R.networkView $ pass <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
+            void $ R.networkView $ (void $ R.networkView $ pure () <$ dragDyn) <$ pure ()
           )
       stateChanges <- pure $ R.traceEventWith (const "StateChange") stateChanges'
   pure ()
+
+partitionEithersNE :: NonEmpty (Either a b) -> These (NonEmpty a) (NonEmpty b)
+partitionEithersNE (x :| xs) = case (x, ls, rs) of
+  (Left  y, ys    , []    ) -> This (y :| ys)
+  (Left  y, ys    , z : zs) -> These (y :| ys) (z :| zs)
+  (Right z, []    , zs    ) -> That (z :| zs)
+  (Right z, y : ys, zs    ) -> These (y :| ys) (z :| zs)
+  where (ls, rs) = partitionEithers xs
